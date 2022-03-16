c67f6bc201b2f7fd13cf43ad3360661c97e724af
Akash52 <ac8572611@gmail.com>
2022-03-16 08:54:15 +0530

    project files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+// import TabApp from './08_Tab/TabApp';
+// import TodoList from './06_Todo/TodoList';
+// import TodoList2 from './07_Todo/TodoList2';
+// import MenuApp from "./05_Menu/MenuApp";
+// import TourApp from "./02_Tours/TourApp";
+// import ReviewsApp from "./03_Reviews/ReviewsApp";
+// import AccordionApp from "./04_accordion/AccordionApp";
+// import Birthday from "./01_Birthday_Remainder/Birthday";
+// import SliderApp from './09_Slider/SliderApp';
+import ShoppingApp from './Shopping/ShoppingApp';
+import UserList from './Table/UserList';
+
+export default function App() {
+  return (
+    <div>
+      {/* <Birthday />; */}
+      {/* <TourApp /> */}
+      {/* <ReviewsApp /> */}
+      {/* <AccordionApp /> */}
+      {/* <MenuApp /> */}
+      {/* <TodoList /> */}
+      {/* <TodoList2 /> */}
+      {/* <TabApp /> */}
+      {/* <SliderApp /> */}
+      {/* <ShoppingApp /> */}
+      <UserList />
+    </div>
+  );
+}
